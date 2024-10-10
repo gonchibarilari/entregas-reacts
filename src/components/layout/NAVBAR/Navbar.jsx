dce0ab2d03e09b9296f238b1a80bdcb48534a00b
gonchibarilari <71528610+gonchibarilari@users.noreply.github.com>
2024-10-10 16:36:34 -0300

    creacion de array con articulos

--- src/components/layout/NAVBAR/Navbar.jsx
+++ src/components/layout/NAVBAR/Navbar.jsx
@@ -1,20 +1,22 @@
+import { CartWidget } from "../../common/cartWidget/CartWidget";
 import "./navbar.css";
-import { FaShoppingCart } from "react-icons/fa";
-
 export const Navbar = () => {
   return (
     <>
       <div className="container-navbar">
-        <h2>Nombr22e de la tienda</h2>
-        <ul>
-          <li>Shampoo</li>
-          <li>ACONDICIONADOR</li>
-          <li>CREMAS</li>
-        </ul>
+        <img
+          src="https://img.bekiabelleza.com/marcas/th/0000/300.jpg"
+          alt="logo"
+        />
+        <h2>ALISARTE TUCUMAN</h2>
         <div>
-          <FaShoppingCart />
-          <span>0</span>
+          <ul className="link-navbar">
+            <li>SHAMPOO</li>
+            <li>ACONDICIONADOR</li>
+            <li>CREMAS</li>
+          </ul>
         </div>
+        <CartWidget />
       </div>
     </>
   );
--- src/components/pages/Card.jsx
+++ src/components/pages/Card.jsx
@@ -12,3 +12,41 @@ export const Card = () => {
 };
 
 export default Card;
+
+const articulos = [
+  {
+    id: 1,
+    nombre: "Camiseta",
+    precio: 29.99,
+    categoria: "Ropa",
+    stock: 50,
+  },
+  {
+    id: 2,
+    nombre: "Gorra",
+    precio: 15.49,
+    categoria: "Accesorios",
+    stock: 100,
+  },
+  {
+    id: 3,
+    nombre: "Zapatillas",
+    precio: 45.0,
+    categoria: "Calzado",
+    stock: 20,
+  },
+  {
+    id: 4,
+    nombre: "Cojín",
+    precio: 9.99,
+    categoria: "Hogar",
+    stock: 75,
+  },
+  {
+    id: 5,
+    nombre: "Auriculares",
+    precio: 34.99,
+    categoria: "Electrónica",
+    stock: 30,
+  },
+];
