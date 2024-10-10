1a1b44ce53540f3b4357b2c58f71748087006a21
gonchibarilari <71528610+gonchibarilari@users.noreply.github.com>
2024-10-10 16:53:50 -0300

    color para la navbar y tamaño de la imagen

--- src/components/layout/NAVBAR/Navbar.jsx
+++ src/components/layout/NAVBAR/Navbar.jsx
@@ -1,19 +1,27 @@
+import { WiDayThunderstorm } from "react-icons/wi";
 import { CartWidget } from "../../common/cartWidget/CartWidget";
 import "./navbar.css";
+// Para
 export const Navbar = () => {
+  const stylesImage = {
+    margin: "10px",
+    width: "50px",
+  };
   return (
     <>
       <div className="container-navbar">
         <img
           src="https://img.bekiabelleza.com/marcas/th/0000/300.jpg"
           alt="logo"
+          style={stylesImage}
         />
         <h2>ALISARTE TUCUMAN</h2>
         <div>
           <ul className="link-navbar">
-            <li>SHAMPOO</li>
-            <li>ACONDICIONADOR</li>
-            <li>CREMAS</li>
+            <li>INICIO</li>
+            <li>PRODUCTOS</li>
+            <li>SERVICIOS</li>
+            <li>CONOCENOS</li>
           </ul>
         </div>
         <CartWidget />
